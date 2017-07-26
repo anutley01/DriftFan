b2054c6880e817456246db7fbf8ae86f410f9e0f
anutley01 <anutley01@qub.ac.uk>
2017-07-26 15:39:04 +0100

    Photo Ticket Home Fragment IDC Logo. Changed Background to black
    App still slow need to change the getImageResources this evening.

--- app/src/main/java/com/example/aids/a09application/HomeFragment.java
+++ app/src/main/java/com/example/aids/a09application/HomeFragment.java
@@ -2,16 +2,127 @@ package com.example.aids.a09application;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
+
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
+    LinearLayout sliderDotsPanel;
+    private int dotsCount;
+    private ImageView[] dots;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+        sliderDotsPanel = (LinearLayout) view.findViewById(R.id.SliderDots);
+
+
+        adapter = new ViewPagerAdapter(this.getActivity());
+        viewPager.setAdapter(adapter);
+
+        dotsCount = adapter.getCount();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+            sliderDotsPanel.addView(dots[i], params);
+
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for (int i = 0; i < dotsCount; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new myTimerTask(), 4000, 2000);
+        return view;
+
+
+    }
+
+
+    public class myTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    while (true) {
+
+                        if (viewPager.getCurrentItem() == 0) {
+                            viewPager.setCurrentItem(1);
+                        } else if (viewPager.getCurrentItem() == 1) {
+                            viewPager.setCurrentItem(2);
+                        } else if (viewPager.getCurrentItem() == 2) {
+                            viewPager.setCurrentItem(1);
+                        } else if (viewPager.getCurrentItem() == 1) {
+                            viewPager.setCurrentItem(0);
+
+                        }
+
+                    }
+
+                }
+
+
+            });
+        }
     }
 }
+
+
+
+
+
+
 
--- app/src/main/java/com/example/aids/a09application/MainActivity.java
+++ app/src/main/java/com/example/aids/a09application/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -69,6 +71,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.drawer_menu, menu);
+        return true;
+
+
+
     }
 }
 
