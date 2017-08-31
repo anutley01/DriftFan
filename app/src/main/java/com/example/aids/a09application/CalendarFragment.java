fdd4fcadfeee6aaebeb0c6036cd227b958f3a73c
anutley01 <anutley01@qub.ac.uk>
2017-08-31 22:14:09 +0100

    Calendar Updated.

--- app/src/main/java/com/example/aids/a09application/CalendarFragment.java
+++ app/src/main/java/com/example/aids/a09application/CalendarFragment.java
@@ -1,23 +1,74 @@
 package com.example.aids.a09application;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /**
  * Created by Aids on 24/08/2017.
  */
 
+        import android.content.Context;
+        import android.graphics.Color;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.app.ActionBar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Toast;
+
+        import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+        import com.github.sundeepk.compactcalendarview.domain.Event;
+
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
+        import java.util.Locale;
+
+        import static com.facebook.FacebookSdk.getApplicationContext;
+
 public class CalendarFragment extends Fragment {
 
-    @Override
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate( R.layout.fragment_calendar, container, false );
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        final ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendar = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //Set an event for Teachers' Professional Day 2016 which is 21st of October
+
+        Event ev1 = new Event(Color.RED, 1477040400000L, "Teachers' Professional Day");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+
+                if (dateClicked.toString().compareTo("Fri Oct 21 00:00:00 AST 2016") == 0) {
+                    Toast.makeText(context, "Teachers' Professional Day", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
+
         return view;
     }
+
 
 }
