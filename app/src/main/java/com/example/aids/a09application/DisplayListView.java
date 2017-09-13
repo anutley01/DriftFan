f73ba6fe86fbfbb0ae3e18d2f1472cfe595c7773
anutley01 <anutley01@qub.ac.uk>
2017-09-13 10:09:13 +0100

    DisplayListView

--- app/src/main/java/com/example/aids/a09application/DisplayListView.java
+++ app/src/main/java/com/example/aids/a09application/DisplayListView.java
@@ -1,14 +1,16 @@
 package com.example.aids.a09application;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ListView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DisplayListView extends Fragment {
@@ -18,48 +20,25 @@ public class DisplayListView extends Fragment {
     JSONArray jsonArray;
     StandingsAdapter standingsAdapter;
     ListView listView;
+    WebView webView;
+    String url;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate( R.layout.display_list_view_layout, container, false );
-        listView= (ListView) view.findViewById( R.id.ListViewParse );
-        standingsAdapter = new StandingsAdapter(getActivity(),R.layout.row_layout);
-        listView.setAdapter( standingsAdapter );
-        json_string = getActivity().getIntent().getStringExtra( "json_data" );
+        View view = inflater.inflate(R.layout.display_list_view_layout, container, false);
 
-        try {
-            jsonArray  = new JSONObject(json_string).getJSONArray("server_response");
-
-
-            int count = 0;
-
-            int driver_id, team_id, position, points;
-            String firstName, lastName;
-
-            while(count<jsonArray.length())
-            {
-                JSONObject JO = jsonArray.getJSONObject(count);
-                driver_id = JO.getInt( "Driver_id" );
-                team_id =JO.getInt( "Team_id" );
-                firstName = JO.getString( "First_name" );
-                lastName= JO.getString( "Last_name" );
-                position= JO.getInt( "Position" );
-                points = JO.getInt( "Points" );
-
-                Standings standings = new Standings(driver_id, team_id, firstName, lastName, position, points );
-                standingsAdapter.add( standings );
-                count++;
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
+        webView = (WebView) view.findViewById(R.id.webviewNews);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        url = Uri.parse("http://163.172.142.145/driver_view.php").toString();
+        webView.loadUrl(url);
         return view;
     }
 
 }
+
+
 
